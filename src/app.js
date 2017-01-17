tjade273/browser-solidity

Add editor.getCurrentFileName

src/app.js
@@ -238,7 +238,7 @@ var run = function () {
   }
 
   function activeFileTab () {
-    var name = utils.fileNameFromKey(editor.getCacheFile())
+    var name = editor.getCurrentFileName()
     return $('#files .file').filter(function () { return $(this).find('.name').text() === name })
   }
 
@@ -482,7 +482,7 @@ var run = function () {
 
   function runCompiler () {
     var files = {}
-    var target = utils.fileNameFromKey(editor.getCacheFile())
+    var target = editor.getCurrentFileName()
 
     files[target] = editor.getValue()
 

ethereum/remix-ide

src/app.js
@@ -287,7 +287,7 @@ var run = function () {
     }
 
     if (editor.cacheFileIsPresent()) {
-      var currentFileName = editor.getCacheFile()
+      var currentFileName = editor.getCurrentFileName()
       var active = $('#files .file').filter(function () { return $(this).find('.name').text() === currentFileName })
       active.addClass('active')
       editor.resetSession()
@@ -519,7 +519,7 @@ var run = function () {
 
   function runCompiler () {
     var files = {}
-    var target = editor.getCacheFile()
+    var target = editor.getCurrentFileName()
 
     files[target] = editor.getValue()
 