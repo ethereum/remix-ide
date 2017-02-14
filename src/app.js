9e692a2a7c35e54eb0e4fcddeb7e65f2d7a55789
Alex Beregszaszi <alex@rtfs.hu>
2017-02-14 19:51:09 +0000

    Add personal mode in the UI

--- src/app.js
+++ src/app.js
@@ -644,7 +644,8 @@ var run = function () {
   // ----------------- UniversalDApp -----------------
   var udapp = new UniversalDApp(executionContext, {
     removable: false,
-    removable_instances: true
+    removable_instances: true,
+    personalMode: $('#personalMode').val() === 'true'
   })
 
   udapp.event.register('debugRequested', this, function (txResult) {
