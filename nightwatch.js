96a8d41e632f5239ac4a00da5e9c0d62d4ee57ef
yann300 <yann.levreau@gmail.com>
2017-04-27 15:55:34 +0200

    change platform traget

--- nightwatch.js
+++ nightwatch.js
@@ -63,8 +63,8 @@ module.exports = {
         'browserName': 'internet explorer',
         'javascriptEnabled': true,
         'acceptSslCerts': true,
-        'platform': 'Windows 8.1',
-        'version': '11.0',
+        'platform': 'Windows 10',
+        'version': '11.103',
         'build': 'build-' + TRAVIS_JOB_NUMBER,
         'tunnel-identifier': 'browsersolidity_tests_' + TRAVIS_JOB_NUMBER
       }
