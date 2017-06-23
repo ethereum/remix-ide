920dd5c7f11e0068b42df9e8b507cb72953d00b1
ninabreznik <ninabreznik@gmail.com>
2017-06-23 19:47:13 +0200

    Add compile button to compile

--- src/app/compile-tab.js
+++ src/app/compile-tab.js
@@ -11,15 +11,55 @@ var css = csjs`
   }
   .contract {
     display: block;
-    margin: 0 0 2em 0;
+    margin: 5% 0;
+  }
+  .compile {
+    display: flex;
+    align-items: flex-start;
+    flex-wrap: wrap;
+  }
+  .autocompile {
+    float: left;
+    align-self: center;
+  }
+  .compilationWarning extends ${styles.warningTextBox} {
+    margin-top: 5%;
+  }
+  .button extends ${styles.button} {
+    background-color: #C6CFF7;
+    width: 30%;
+    text-align: -webkit-center;
   }
 `
+
+// HELPERS
 
 module.exports = compileTab
 
 function compileTab (container, appAPI, appEvents, opts) {
+  if (typeof container === 'string') container = document.querySelector(container)
+  if (!container) throw new Error('no container given')
+  var warnCompilationSlow = yo`<div id="warnCompilationSlow"></div>`
+  appEvents.compiler.register('compilationDuration', function tabHighlighting (speed) {
+    var settingsView = document.querySelector('#header #menu .settingsView')
+    warnCompilationSlow.className = css.compilationWarning
+    if (speed > 1000) {
+      warnCompilationSlow.innerHTML = `Last compilation took ${speed}ms. We suggest to turn off autocompilation.`
+      warnCompilationSlow.style.visibility = 'visible'
+      settingsView.style.color = '#FF8B8B'
+    } else {
+      warnCompilationSlow.innerHTML = ''
+      warnCompilationSlow.style.visibility = 'hidden'
+      settingsView.style.color = ''
+    }
+  })
   var el = yo`
     <div class="${css.compileTabView}" id="compileTabView">
+      <div class="${css.compile}">
+        <input class="${css.autocompile}" id="autoCompile" type="checkbox" checked title="Auto compile">
+        <div class="${css.button} "id="compile" title="Compile source code">Compile</div>
+        ${warnCompilationSlow}
+      </div>
       <div id="output" class="${css.contract}"></div>
     </div>
   `
--- src/app/settings-tab.js
+++ src/app/settings-tab.js
@@ -21,23 +21,6 @@ var css = csjs`
     float: left;
     max-width: 90%;
   }
-  .button extends ${styles.button} {
-    background-color: #C6CFF7;
-    width: 100%;
-    align-self: center;
-    text-align: -webkit-center;
-  }
-  .col1 extends ${styles.titleL} {
-    float: left;
-    align-self: center;
-  }
-  .checkboxText {
-    margin-left: 3px;
-  }
-  .compilationWarning extends ${styles.warningTextBox} {
-    margin-top: 1em;
-    margin-left: 0.5em;
-  }
 }
 `
 module.exports = SettingsTab
@@ -45,21 +28,6 @@ module.exports = SettingsTab
 function SettingsTab (container, appAPI, appEvents, opts) {
   if (typeof container === 'string') container = document.querySelector(container)
   if (!container) throw new Error('no container given')
-
-  var warnCompilationSlow = yo`<div id="warnCompilationSlow"></div>`
-  warnCompilationSlow.className = css.compilationWarning
-  appEvents.compiler.register('compilationDuration', function tabHighlighting (speed) {
-    var settingsView = document.querySelector('#header #menu .settingsView')
-    if (speed > 1000) {
-      warnCompilationSlow.innerHTML = `Last compilation took ${speed}ms. We suggest to turn off autocompilation.`
-      warnCompilationSlow.style.visibility = 'visible'
-      settingsView.style.color = '#FF8B8B'
-    } else {
-      warnCompilationSlow.innerHTML = ''
-      warnCompilationSlow.style.visibility = 'hidden'
-      settingsView.style.color = ''
-    }
-  })
 
   var el = yo`
     <div class="${css.settingsTabView} "id="settingsView">
@@ -70,23 +38,6 @@ function SettingsTab (container, appAPI, appEvents, opts) {
       <div class="${css.crow}">
         <select class="${css.select}" id="versionSelector"></select>
       </div>
-      <div class="${css.crow}">
-        <div><input class="${css.col1}" id="editorWrap" type="checkbox"></div>
-        <span class="${css.checkboxText}">Text Wrap</span>
-      </div>
-      <div class="${css.crow}">
-        <div><input class="${css.col1}" id="optimize" type="checkbox"></div>
-        <span class="${css.checkboxText}">Enable Optimization</span>
-      </div>
-      <div class="${css.crow}">
-        <div><input class="${css.col1}" id="autoCompile" type="checkbox" checked></div>
-        <span class="${css.checkboxText}">Auto Compile</span>
-      </div>
-      </div>
-      <div class="${css.crow}">
-        <div class="${css.button} "id="compile" title="Compile source code">Compile</div>
-      </div>
-      ${warnCompilationSlow}
     </div>
   `
   container.appendChild(el)
