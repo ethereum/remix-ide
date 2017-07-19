7fd91ff593d83673dcd6f230b491d38227dd27ec
yann300 <yann.levreau@gmail.com>
2017-07-19 17:46:02 +0200

    extract publishOnSwarm

--- src/app/compile-tab.js
+++ src/app/compile-tab.js
@@ -2,10 +2,9 @@
 var $ = require('jquery')
 
 var yo = require('yo-yo')
-var async = require('async')
-var swarmgw = require('swarmgw')
 
 var parseContracts = require('./contract/contractParser')
+var publishOnSwarm = require('./contract/publishOnSwarm')
 
 // -------------- styling ----------------------
 var csjs = require('csjs-inject')
@@ -264,7 +263,7 @@ function compileTab (container, appAPI, appEvents, opts) {
     function publish (appAPI) {
       var selectContractNames = document.querySelector(`.${css.contractNames.classNames[0]}`)
       var contract = contractsDetails[selectContractNames.children[selectContractNames.selectedIndex].innerText]
-      publishOnSwarm(contract, function (err) {
+      publishOnSwarm(contract, appAPI, function (err) {
         if (err) {
           alert('Failed to publish metadata: ' + err)
         } else {
@@ -272,67 +271,6 @@ function compileTab (container, appAPI, appEvents, opts) {
         }
       })
     }
-
-    function publishOnSwarm (contract, cb) {
-      // gather list of files to publish
-      var sources = []
-
-      sources.push({
-        content: contract.metadata,
-        hash: contract.metadataHash
-      })
-
-      var metadata
-      try {
-        metadata = JSON.parse(contract.metadata)
-      } catch (e) {
-        return cb(e)
-      }
-
-      if (metadata === undefined) {
-        return cb('No metadata')
-      }
-
-      async.eachSeries(Object.keys(metadata.sources), function (fileName, cb) {
-        // find hash
-        var hash
-        try {
-          hash = metadata.sources[fileName].urls[0].match('bzzr://(.+)')[1]
-        } catch (e) {
-          return cb('Metadata inconsistency')
-        }
-
-        appAPI.fileProviderOf(fileName).get(fileName, (error, content) => {
-          if (error) {
-            console.log(error)
-          } else {
-            sources.push({
-              content: content,
-              hash: hash
-            })
-          }
-          cb()
-        })
-      }, function () {
-        // publish the list of sources in order, fail if any failed
-        async.eachSeries(sources, function (item, cb) {
-          swarmVerifiedPublish(item.content, item.hash, cb)
-        }, cb)
-      })
-      function swarmVerifiedPublish (content, expectedHash, cb) {
-        swarmgw.put(content, function (err, ret) {
-          if (err) {
-            cb(err)
-          } else if (ret !== expectedHash) {
-            cb('Hash mismatch')
-          } else {
-            cb()
-          }
-        })
-      }
-    }
-
-
     return el
   }
 }
