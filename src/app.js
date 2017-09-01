0501f62dacf04cbf6c7696737c2e4ba13d1dcceb
Rob Stupay <rob@stupay.com>
2017-09-01 14:50:27 +0200

    fixing a few standard js error due to spaces

--- src/app.js
+++ src/app.js
@@ -634,8 +634,6 @@ function run () {
     editor.clearAnnotations()
   })
 
-
-
   function startdebugging (txHash) {
     self.event.trigger('debuggingRequested', [])
     transactionDebugger.debug(txHash)
