c72562aee48bc73879ecb3c3fb0a68515fde0664
Jernej <j@eee.si>
2020-01-20 17:20:48 +0100

    use spaces

--- test-browser/tests/import.js
+++ test-browser/tests/import.js
@@ -6,35 +6,35 @@ const sauce = require('./sauce')
 const gistid = 'ceb44cee32b2073e4d0d5ec5729cc9d2';
 
 module.exports = {
-	before: function (browser, done) {
-		init(browser, done);
-	},
-	'From Gist URL success': function (browser) {
-		browser
-			.waitForElementVisible('#icon-panel', 10000)
-			.scrollAndClick('.file .btn-group button:nth-of-type(1)') // this selector is a bit dangeours
-			.waitForElementVisible('#modal-footer-ok', 4000)
-			.setValue('input[id="prompt_text"]', `https://gist.github.com/jernejc/${gistid}`)
-			.modalFooterOKClick()
-			.switchFile('browser/gists')
-			.switchFile(`browser/gists/${gistid}`)
-			.switchFile(`browser/gists/${gistid}/SimpleStorage.sol`)
-	},
-	'From Gist failed': function (browser) {
-		browser
-			.clickLaunchIcon('home')
-			.scrollAndClick('.file .btn-group button:nth-of-type(1)')
-			.waitForElementVisible('#modal-footer-ok', 6000)
-			.setValue('input[id="prompt_text"]', `https://gist.github.com/jernejc/${gistid.slice(0, gistid.length - 2)}`)
-			.modalFooterOKClick()
-			.waitForElementVisible('#modal-footer-ok', 6000)
-			.getText('.modal-body', (result) => {
-				browser.assert.equal(result.value, 'Gist load error: Not Found');
-			})
-	},
-	after: (browser, done) => {
-		browser.end();
-		done();
-	},
-	tearDown: sauce
+  before: function (browser, done) {
+    init(browser, done);
+  },
+  'From Gist URL success': function (browser) {
+    browser
+      .waitForElementVisible('#icon-panel', 10000)
+      .scrollAndClick('.file .btn-group button:nth-of-type(1)') // this selector is a bit dangeours
+      .waitForElementVisible('#modal-footer-ok', 4000)
+      .setValue('input[id="prompt_text"]', `https://gist.github.com/jernejc/${gistid}`)
+      .modalFooterOKClick()
+      .switchFile('browser/gists')
+      .switchFile(`browser/gists/${gistid}`)
+      .switchFile(`browser/gists/${gistid}/SimpleStorage.sol`)
+  },
+  'From Gist failed': function (browser) {
+    browser
+      .clickLaunchIcon('home')
+      .scrollAndClick('.file .btn-group button:nth-of-type(1)')
+      .waitForElementVisible('#modal-footer-ok', 6000)
+      .setValue('input[id="prompt_text"]', `https://gist.github.com/jernejc/${gistid.slice(0, gistid.length - 2)}`)
+      .modalFooterOKClick()
+      .waitForElementVisible('#modal-footer-ok', 6000)
+      .getText('.modal-body', (result) => {
+        browser.assert.equal(result.value, 'Gist load error: Not Found');
+      })
+  },
+  after: (browser, done) => {
+    browser.end();
+    done();
+  },
+  tearDown: sauce
 }
