6aecadb58fb232bad613ae05cf09f4b9cba3d64b
Iuri Matias <iuri.matias@gmail.com>
2020-02-07 18:34:22 -0500

    use web3 directly in injected and node providers

--- src/blockchain/providers/injected.js
+++ src/blockchain/providers/injected.js
@@ -1,20 +1,22 @@
 const Web3 = require('web3')
-const { stripHexPrefix, hashPersonalMessage } = require('ethereumjs-util')
+const { stripHexPrefix } = require('ethereumjs-util')
 
 class InjectedProvider {
 
   constructor (executionContext) {
     this.executionContext = executionContext
+    this.web3 = new Web3(this.executionContext.internalWeb3().givenProvider)
   }
 
   getAccounts (cb) {
-    return this.executionContext.web3().eth.getAccounts(cb)
+    return this.web3.eth.getAccounts(cb)
   }
 
   newAccount (passwordPromptCb, cb) {
-    passwordPromptCb((passphrase) => {
-      this.executionContext.web3().personal.newAccount(passphrase, cb)
-    })
+    throw new Error("not allowed in an injected provider")
+    // passwordPromptCb((passphrase) => {
+      // this.executionContext.web3().personal.newAccount(passphrase, cb)
+    // })
   }
 
   resetEnvironment () {
@@ -22,27 +24,28 @@ class InjectedProvider {
 
   getBalanceInEther (address, cb) {
     address = stripHexPrefix(address)
-    this.executionContext.web3().eth.getBalance(address, (err, res) => {
+    this.web3.eth.getBalance(address, (err, res) => {
       if (err) {
         return cb(err)
       }
-      cb(null, Web3.utils.fromWei(res.toString(10), 'ether'))
+      // cb(null, Web3.utils.fromWei(res.toString(10), 'ether'))
+      cb(null, Web3.utils.fromWei(new BN(res).toString(10), 'ether'))
     })
   }
 
   getGasPrice (cb) {
-    this.executionContext.web3().eth.getGasPrice(cb)
+    this.web3.eth.getGasPrice(cb)
   }
 
   signMessage (message, account, _passphrase, cb) {
-    const messageHash = hashPersonalMessage(Buffer.from(message))
-    try {
-      this.executionContext.web3().eth.sign(message, account, (error, signedData) => {
-        cb(error, '0x' + messageHash.toString('hex'), signedData)
-      })
-    } catch (e) {
-      cb(e.message)
-    }
+    const hashedMsg = Web3.utils.sha3(message)
+
+    this.web3.eth.sign(hashedMsg, account, (error, signedData) => {
+      if (error) {
+        return cb(error)
+      }
+      cb(null, hashedMsg, signedData)
+    })
   }
 
   getProvider () {
