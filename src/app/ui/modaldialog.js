1090ddfdeeec59d6d1d5512b3482ab3284a71fb6
ioedeveloper <daviddisu8@gmail.com>
2020-02-14 09:33:07 +0000

    Merged importFromGist.js into gist.js and changed selectors to data-attributes

--- src/app/ui/modaldialog.js
+++ src/app/ui/modaldialog.js
@@ -134,12 +134,12 @@ function html (opts) {
     <div id="modal-background" class="modal-dialog" role="document">
       <div class="modal-content ${css.modalContent} ${opts.class}">
         <div class="modal-header">
-          <h6 class="modal-title"></h6>
+          <h6 class="modal-title" data-id="modalDialogModalTitle"></h6>
           <span class="modal-close">
             <i id="modal-close" title="Close" class="fas fa-times" aria-hidden="true"></i>
           </span>
         </div>
-        <div class="modal-body ${css.modalBody}"> - </div>
+        <div class="modal-body ${css.modalBody}" data-id="modalDialogModalBody"> - </div>
         <div class="modal-footer" autofocus>
           <span id="modal-footer-ok" class="${css['modalFooterOk']} modal-ok btn btn-sm btn-light" tabindex='5'>OK</span>
           <span id="modal-footer-cancel" class="${css['modalFooterCancel']} modal-cancel btn btn-sm btn-light" tabindex='10' data-dismiss="modal">Cancel</span>
