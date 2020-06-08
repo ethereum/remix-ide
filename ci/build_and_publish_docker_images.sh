1bf13fd93d4176bd9a0d966375fa7c4f09d3002f
yann300 <yann.levreau@gmail.com>
2020-06-08 11:57:35 +0200

    add docker

---
+++ ci/build_and_publish_docker_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# If not staging and master branch are existing
+export TAG="$CIRCLE_BRANCH"
+
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker build -f Dockerfile -t remixproject/remix-ide:$TAG
+docker push remixproject/remix-ide:$TAG
