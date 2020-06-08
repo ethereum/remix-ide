32e08dde066126b5de4e94f3e7a85f458decc2ef
yann300 <yann.levreau@gmail.com>
2020-06-08 11:14:11 +0200

    Update build_and_publish_docker_images.sh

--- ci/build_and_publish_docker_images.sh
+++ ci/build_and_publish_docker_images.sh
@@ -4,8 +4,6 @@ set -e
 # If not staging and master branch are existing
 export TAG="$CIRCLE_BRANCH"
 
-echo $SERVICE
-
 docker login --username $DOCKER_USER --password $DOCKER_PASS
 docker build -f Dockerfile -t remixproject/remix-ide:$SERVICE-$TAG
-docker push remixproject/remix-ide:$SERVICE-$TAG
+docker push remixproject/remix-ide:$TAG
