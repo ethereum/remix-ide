4b0e074fe090236f217f523f11932df8f77189c7
yann300 <yann.levreau@gmail.com>
2020-07-15 12:16:48 +0200

    remove console.log

--- src/app/tabs/runTab/model/dropdownlogic.js
+++ src/app/tabs/runTab/model/dropdownlogic.js
@@ -33,7 +33,6 @@ class DropdownLogic {
     )
     this.runView.on('lexon', 'compilationFinished', (file, source, languageVersion, data) =>
       broadcastCompilationResult(file, source, languageVersion, data)
-      // console.log('broadcasting compilation result ', file, source, languageVersion, data)
     )
     this.runView.on('yulp', 'compilationFinished', (file, source, languageVersion, data) =>
       broadcastCompilationResult(file, source, languageVersion, data)
