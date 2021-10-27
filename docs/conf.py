ceabd1a733ea2094b9a40538469fba8d8831d791
yann300 <yann.levreau@gmail.com>
2021-10-27 16:09:09 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -230,10 +230,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-# latex_documents = [
-#    (master_doc, 'Remix.tex', u'Remix Documentation',
-#     u'yann300', 'manual'),
-# ]
+latex_documents = [
+    (master_doc, 'Remix.tex', u'Remix Documentation',
+     u'yann300', 'manual'),
+]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
