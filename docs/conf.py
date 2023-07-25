92fd4f249b25c9157986dcd2167509937e815130
ryestew <rob@stupay.com>
2023-07-25 11:12:35 +0200

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
