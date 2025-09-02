f20400cf50a29d8c0b32ae24034fefdb9ac65e02
davidekete <eketedavid@gmail.com>
2025-09-02 12:14:25 +0100

    tooling: add copybutton for code blocks and improve syntax highlighting for darkmode

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ import datetime
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser'] # required for sphinx v3.0.0
+extensions = ['myst_parser', 'sphinx_copybutton'] # required for sphinx v3.0.0
+copybutton_prompt_text = r">>> |\$ "
+copybutton_prompt_is_regexp = True
 
 # suppress warnings caused by non-consecutive header
 # see more details here https://myst-parser.readthedocs.io/en/latest/faq/index.html#suppress-warnings
@@ -118,7 +120,7 @@ exclude_patterns = [
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-
+pygments_dark_style = "native"
 highlight_language = 'JavaScript'
 
 # A list of ignored prefixes for module index sorting.
